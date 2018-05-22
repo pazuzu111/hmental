43e3a0f89a0f4e451737081e5933960df5d89f47
pazuzu111 <pazuzublue111@gmail.com>
2018-05-21 22:00:12 -0400

    added react-router & created links

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import { Route, Link } from 'react-router-dom'
 import './App.css';
 
-class App extends Component {
+export default  class App extends Component {
   render() {
     return (
       <div className="App">
@@ -10,7 +11,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+
         <p className="App-intro">
+
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
@@ -18,4 +21,14 @@ class App extends Component {
   }
 }
 
-export default App;
+const nav = () => {
+    return (
+        <div>
+            <ul>
+              <li><Link to="/"> purpose </Link></li>
+              <li><Link to="/about"> dapp </Link></li>
+              <li><Link to="/topics"> whitepaper </Link></li>
+            </ul>
+        </div>
+    )
+}
